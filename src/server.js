4b0cdf69b05efcba941fa5ceadcca3ef4a3547b7
Jonatan Daniel Avila <121068399+JONAAVILA@users.noreply.github.com>
2024-09-30 16:18:45 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { limiterCodes,limiterLogin } from './utils/limiter.js';
 
 const server = express();
 const corsOptions = {
-    origin: ['http://localhost:4321/', 'https://tienda-online-backend-riqf.onrender.com/'], // Agrega los dominios permitidos
+    origin: ['http://localhost:4321', 'https://tienda-online-backend-riqf.onrender.com'], // Agrega los dominios permitidos
     methods: ['GET', 'POST'], // Agrega los métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Agrega los headers permitidos
 };
