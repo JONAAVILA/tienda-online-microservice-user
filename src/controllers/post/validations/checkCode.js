ea9bbf0e06c85594b0e274ab4bcc405e44a68a49
JONAAVILA <jona_1969@hotmail.com>
2024-10-10 12:44:19 -0300

    cookies aplieted to checkcode

--- src/controllers/post/validations/checkCode.js
+++ src/controllers/post/validations/checkCode.js
@@ -4,6 +4,7 @@ const checkCode = async (req,res)=>{
     try {
         const token = req.cookies['access-token']
         const { code } = req.body
+
         const check = await handleCheckCode(code,token)
         res.status(200).json(check)
     } catch (error) {
