c5c471dea6f8bf33c6a5fa245ff495365b9b9ea3
JONAAVILA <jona_1969@hotmail.com>
2024-11-08 10:52:18 -0300

    fixed cookie login

--- src/controllers/post/validations/Login.js
+++ src/controllers/post/validations/Login.js
@@ -10,7 +10,7 @@ const login = async (req,res)=>{
         res.cookie('validate-token',access.token,{
             httpOnly:true,
             secure:false,
-            sameSite:'strict',
+            sameSite:'lax',
             path:'/',
             maxAge: 6 * 60 * 1000
         })
