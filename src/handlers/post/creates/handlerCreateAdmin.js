ba51a79e4a5b61fb068c255e1dc56b1669753496
JONAAVILA <jona_1969@hotmail.com>
2024-12-13 01:07:41 -0300

    validation parameters handler create admin fixed

--- src/handlers/post/creates/handlerCreateAdmin.js
+++ src/handlers/post/creates/handlerCreateAdmin.js
@@ -7,52 +7,54 @@ const { Admin } = models
 const SALT_ROUNDS = Number(process.env)
 
 const handlerCreateAdmin = async (id,name,surname,seller,email,password)=>{
-    const { error } = schema.validate({
-        uuid:id,
-        name:name,
-        surname:surname,
-        seller:seller,
-        email:email,
-        password:password
-    })
-    if(error) throw new Error("Parametros inválidos");
-    
-    const passwordHashed = bcrypt.hashSync(password, SALT_ROUNDS)
-
-    const [ admin, created ] = await Admin.findOrCreate({
-        where:{
-            seller:seller,
-            email:email
-        },
-        defaults:{
-            id:id,
+    if(id && name && surname && seller && email && password ){
+        const { error } = schema.validate({
+            uuid:id,
             name:name,
             surname:surname,
             seller:seller,
             email:email,
-            password:passwordHashed
-        }
-    })
-    if(!created) throw new Error("Administrator already exists");
+            password:password
+        })
+        if(error) throw new Error("Parametros inválidos");
+        
+        const passwordHashed = bcrypt.hashSync(password, SALT_ROUNDS)
     
-    const adminResponse = {
-        id:admin.id,
-        name:admin.name,
-        surname:admin.surname
-    }
-
-    const refresToken = userRefrestJwt(
-        admin.id
-    )
-    const LoginJwt = userLoginJwt(
-        admin.email
-    )
-
-    return {
-        refresToken,
-        LoginJwt,
-        adminResponse
-    }
+        const [ admin, created ] = await Admin.findOrCreate({
+            where:{
+                seller:seller,
+                email:email
+            },
+            defaults:{
+                id:id,
+                name:name,
+                surname:surname,
+                seller:seller,
+                email:email,
+                password:passwordHashed
+            }
+        })
+        if(!created) throw new Error("Administrator ya existe 🤷‍♀️");
+        
+        const adminResponse = {
+            id:admin.id,
+            name:admin.name,
+            surname:admin.surname
+        }
+    
+        const refresToken = userRefrestJwt(
+            admin.id
+        )
+        const LoginJwt = userLoginJwt(
+            admin.email
+        )
+    
+        return {
+            refresToken,
+            LoginJwt,
+            adminResponse
+        }
+    }throw new Error('Parametros perdidos 🤦‍♂️');
 }
 
 export default handlerCreateAdmin
